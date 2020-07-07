f0dd7649c53e85447c82ed0e065a481ef5e9f707
Jesse Henson <jesse.henson@staffscapes.com>
2020-07-07 15:02:38 -0600

    som

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+var xml2js = require("xml2js");
+
 export default (req, res) => {
   if (req.method === "POST") {
     console.log(res);
+    res.status(200).json('worked')
   } else {
     console.log("worked");
     res.status(200).json("worked");
