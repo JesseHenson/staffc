edd7e4a619ab4d90943c51cf22f653dd6b871848
Jesse Henson <jesse.henson@staffscapes.com>
2020-07-07 15:14:05 -0600

    som new

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,7 +4,7 @@ var xml2js = require("xml2js");
 
 export default (req, res) => {
   if (req.method === "POST") {
-    console.log("we'll grab the data from here");
+    console.log("we'll grab the data from hereasda");
     res.status(200).json("worked");
   } else {
     console.log("worked");
