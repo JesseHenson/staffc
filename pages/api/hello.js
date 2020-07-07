efb0520241066338d6dfc23f1c060249f18a37c7
Jesse Henson <jesse.henson@staffscapes.com>
2020-07-07 14:42:21 -0600

    just log it

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,10 +2,9 @@
 
 export default (req, res) => {
   if (req.method === "POST") {
+    console.log(res);
+  } else {
     console.log("worked");
     res.status(200).json("worked");
-  } else {
-    res.status(200).json("worked");
-    res.status(200);
   }
 };
